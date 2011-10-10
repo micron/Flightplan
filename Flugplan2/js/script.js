676a60601e7fb4c1e740a2e484efeb83dc14d710
miron <miron@wlkp.de>
2011-10-10 17:17:09 +0200

    added some selection options to the flightplan

--- Flugplan2/js/script.js
+++ Flugplan2/js/script.js
@@ -8,11 +8,20 @@ jQuery(function ($) {
 
             $.each(flights, function (key, value) {
                 if (options.limit >= (key + 1)) {
-                    flightsString = flightsString + '<tbody><tr><td>' + value.number + '</td><td>' + value.destination + '</td><td>' + value.time + '</td><td>' + value.rollway + '</td><td>' + value.pilot + '</td><td>' + value.numberplate + '</td></tr></tbody>';
+                    flightsString = flightsString + '<tbody><tr class="ui-widget-content"><td>' + value.number + '</td><td>' + value.destination + '</td><td>' + value.time + '</td><td>' + value.rollway + '</td><td>' + value.pilot + '</td><td>' + value.numberplate + '</td></tr></tbody>';
+                }
+
+                if (typeof options.search == 'number' && key == options.search) {
+                    flightsString = flightsString + '<tbody><tr class="ui-widget-content"><td>' + value.number + '</td><td>' + value.destination + '</td><td>' + value.time + '</td><td>' + value.rollway + '</td><td>' + value.pilot + '</td><td>' + value.numberplate + '</td></tr></tbody>';
                 }
             })
 
-            $('#flights').append(flightsString);
+            var toggleFlights = function () {
+
+            }
+            options.search = null;
+            $('#flights').append(flightsString).show('bounce');
+
         }
     }
 
@@ -36,4 +45,37 @@ jQuery(function ($) {
     });
 
     $("#Text1").val($("#amount").slider("value"));
+
+    var tempArray = [];
+
+    for (var i = 0, c = flights.length; i < c; i++) {
+        $.each(flights[i], function (key, value) {
+
+            var data = {};
+
+            data.value = i;
+            data.label = value;
+            tempArray.push(data);
+
+        })
+    }
+
+    $('#search').autocomplete({
+        source: tempArray,
+        select: function (event, ui) {
+            $("#search").val(ui.item.label);
+            options = {
+                limit: null,
+                search: ui.item.value
+            }
+            outputFlights(flights, options);
+            return false;
+        }
+
+    }).data("autocomplete")._renderItem = function (ul, item) {
+        return $("<li></li>")
+				.data("item.autocomplete", item)
+				.append("<a>" + item.label + "</a>")
+				.appendTo(ul);
+    }
 })
